64d1f61889cdf55c052c61de8e54cb4f0581129c
Ryo Sakamoto <rsakamot.0620@gmail.com>
2014-09-01 06:47:18 +0900

    modified cat-golang.go

--- cat/cat-golang.go
+++ cat/cat-golang.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"os"
 )
 
 func check(e error) {
@@ -21,11 +23,23 @@ func main() {
 	fmt.Println("files: ", files)
 
 	fmt.Println("number flag: ", number)
+
 	for n := range files {
 		file := files[n]
 		fmt.Println("file: ", file)
-		dat, err := ioutil.ReadFile(file)
-		check(err)
-		fmt.Print(string(dat))
+		line := 1
+		if !number {
+			dat, err := ioutil.ReadFile(file)
+			check(err)
+			fmt.Print(string(dat))
+		} else {
+			fp, err := os.Open(file)
+			check(err)
+			scanner := bufio.NewScanner(fp)
+			for scanner.Scan() {
+				fmt.Println(line, " ", scanner.Text())
+				line = line + 1
+			}
+		}
 	}
 }
